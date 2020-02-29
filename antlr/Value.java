d0cf4d6ef35ebde8c802dbe5e41e40e6356cfb5d
Arod <andrew.rodriuges@ufl.edu>
2020-02-29 15:44:45 -0500

    fixed git hub

--- antlr/Value.java
+++ antlr/Value.java
@@ -5,7 +5,6 @@ public class Value
     String name;
     float realValue;
     boolean boolValue;
-<<<<<<< HEAD
    
 
     public Value(String name)
@@ -13,42 +12,21 @@ public class Value
         this.name = name;
     }
     public Value(String name, String type)
-=======
-    
-
-    public Value(String name) 
-    {
-        this.name = name;
-    }
-    public Value(String name, String type) 
->>>>>>> edf0638abd67ed2c12cf04ae90b4d5124130fb18
     {
         this.name = name;
         this.type = type;
     }
-<<<<<<< HEAD
     public Value(String name, String type, float realValue)
-=======
-    public Value(String name, String type, float realValue) 
->>>>>>> edf0638abd67ed2c12cf04ae90b4d5124130fb18
     {
         this.name = name;
         this.type = type;
         this.realValue = realValue;
     }
-<<<<<<< HEAD
     public Value(String name, String type, boolean boolValue)
-=======
-    public Value(String name, String type, boolean boolValue) 
->>>>>>> edf0638abd67ed2c12cf04ae90b4d5124130fb18
     {
         this.name = name;
         this.type = type;
         this.boolValue = boolValue;
     }
-<<<<<<< HEAD
    
-=======
-    
->>>>>>> edf0638abd67ed2c12cf04ae90b4d5124130fb18
 }
